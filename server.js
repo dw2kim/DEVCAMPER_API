21f587ded8794ea6b50758f93788d7a443f16348
Dae Won Kim <dw2kim@gmail.com>
2023-03-19 10:27:50 -0400

    more setup

--- server.js
+++ server.js
@@ -1,14 +1,42 @@
 const express = require("express");
 const dotenv = require("dotenv");
+// const logger = require("./middleware/logger.js");
+const morgan = require("morgan");
+const colors = require("colors");
+const connectDB = require("./config/db");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
 
+// Connect to DB
+connectDB();
+
+// Route files
+const bootcamps = require("./routes/bootcamps");
+
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+// app.use(logger);
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
-app.listen(
+// Mount routers
+app.use("/api/v1/bootcamps", bootcamps);
+
+const PORT = process.env.PORT || 5001;
+
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
+
+// Handle unhandled prmoise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server & exit
+  server.close(() => process.exit(1));
+});
