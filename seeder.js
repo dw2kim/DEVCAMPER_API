75e2e7a4b3a90f762eb946b0a1294326cca1ba1c
Dae Won Kim <dw2kim@gmail.com>
2023-10-30 21:38:48 -0400

    Add Courses

--- seeder.js
+++ seeder.js
@@ -8,6 +8,7 @@ dotenv.config({ path: "./config/config.env" });
 
 // Load Models
 const Bootcamp = require("./models/Bootcamp");
+const Course = require("./models/Course");
 
 // Connect to DB
 mongoose.set("strictQuery", false);
@@ -20,11 +21,15 @@ mongoose.connect(process.env.MONGO_URI, {
 const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
 );
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
+);
 
 // Import into DB
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
+    await Course.create(courses);
 
     console.log("Data imported...".green.inverse);
     process.exit();
@@ -37,6 +42,7 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
+    await Course.deleteMany();
 
     console.log("Data destroyed...".red.inverse);
     process.exit();
