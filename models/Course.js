9c68d1a056ab640bff4c66964265f85f55d2dd95
Dae Won Kim <dw2kim@gmail.com>
2023-11-04 13:05:16 -0400

    Add auth - route, controller and middleware

--- models/Course.js
+++ models/Course.js
@@ -42,8 +42,6 @@ const CourseSchema = new mongoose.Schema({
 
 // Static method to get average of the course tutions
 CourseSchema.statics.getAverageCost = async function (bootcampId) {
-  console.log("Calculating avg cots...".blue);
-
   const obj = await this.aggregate([
     {
       $match: { bootcamp: bootcampId },
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const colors = require("colors");
 const errorHandler = require("./middleware/error");
 const connectDB = require("./config/db");
+const fileupload = require("express-fileupload");
+const cookieParser = require("cookie-parser");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
@@ -14,11 +17,15 @@ connectDB();
 // Route files
 const bootcamps = require("./routes/bootcamps");
 const courses = require("./routes/courses");
+const auth = require("./routes/auth");
 
 const app = express();
 
 // Body parser
 app.use(express.json());
+
+// Cookie parser
+app.use(cookieParser());
 
 // app.use(logger);
 // Dev logging middleware
@@ -26,9 +33,16 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+// File Upload
+app.use(fileupload());
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "Public")));
+
 // Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
 app.use("/api/v1/courses", courses);
+app.use("/api/v1/auth", auth);
 
 app.use(errorHandler);
 
